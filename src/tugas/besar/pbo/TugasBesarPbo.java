2cdbc1b33474cfdca517e4037dff849cdcde04db
MaulanaAhmad <maulana196.ma@gmail.com>
2017-05-19 16:02:25 +0700

    Update TugasBesarPbo.java

--- src/tugas/besar/pbo/TugasBesarPbo.java
+++ src/tugas/besar/pbo/TugasBesarPbo.java
@@ -18,8 +18,8 @@ public class TugasBesarPbo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Tampildata tampil = new Tampildata();
-        tampil.setVisible(true);
+        Tampildata tampl = new Tampildata();
+        tampl.setVisible(true);
     }
     
 }
