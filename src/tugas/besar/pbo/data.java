86355253762018adee191d905881f8123e0d8a01
Maulana Ahmad <Maulana Ahmad@DESKTOP-G3IPT0M>
2017-05-19 15:47:56 +0700

    New commit

---
+++ src/tugas/besar/pbo/data.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.besar.pbo;
+
+public class data {
+    private String NIK;
+    private String nama;
+    private String TglLahir;
+    private String JenisK;
+    private String alamat;
+    private String RT;
+    private String Desa;
+    private String Kec;
+    private String agama;
+    private String status;
+    private String kerja;
+    private String kewarganegaraan;
+
+    public String getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTTL() {
+        return TglLahir;
+    }
+
+    public void setTTL(String TglLahir) {
+        this.TglLahir = TglLahir;
+    }
+
+    public String getJkl() {
+        return JenisK;
+    }
+
+    public void setJkl(String JenisK) {
+        this.JenisK = JenisK;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getRt() {
+        return RT;
+    }
+
+    public void setRt(String RT) {
+        this.RT = RT;
+    }
+
+    public String getDesa() {
+        return Desa;
+    }
+
+    public void setDesa(String Desa) {
+        this.Desa = Desa;
+    }
+
+    public String getKecamatan() {
+        return Kec;
+    }
+
+    public void setKecamatan(String Kec) {
+        this.Kec = Kec;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPekerjaan() {
+        return kerja;
+    }
+
+    public void setPekerjaan(String kerja) {
+        this.kerja = kerja;
+    }
+
+    public String getKewarganegaraan() {
+        return kewarganegaraan;
+    }
+
+    public void setKewarganegaraan(String kewarganegaraan) {
+        this.kewarganegaraan = kewarganegaraan;
+    }
+}
